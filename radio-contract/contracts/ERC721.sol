44bfbc71707aed7e2097b68655ee1a4738c3c6f0
Daniel Shannon <dan.j.shannon@gmail.com>
2022-01-07 22:12:29 -0700

    update radio contract

--- radio-contract/contracts/ERC721.sol
+++ radio-contract/contracts/ERC721.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.8.10;
 
 /// @title ERC-721 Non-Fungible Token Standard
 /// @dev See https://eips.ethereum.org/EIPS/eip-721
--- radio-contract/contracts/Radio.sol
+++ radio-contract/contracts/Radio.sol
@@ -1,8 +1,8 @@
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity ^0.8.10;
 import "./ERC721.sol";
 
 contract Radio is ERC721 {
-    constructor() public {}
+    // constructor() public {}
 
     mapping(uint256 => address) private _owners;
     //owners balances
@@ -13,29 +13,29 @@ contract Radio is ERC721 {
     ///  (`to` == 0). Exception: during contract creation, any number of NFTs
     ///  may be created and assigned without emitting Transfer. At the time of
     ///  any transfer, the approved address for that NFT (if any) is reset to none.
-    event Transfer(
-        address indexed _from,
-        address indexed _to,
-        uint256 indexed _tokenId
-    );
+    // event Transfer(
+    //     address indexed _from,
+    //     address indexed _to,
+    //     uint256 indexed _tokenId
+    // );
 
     /// @dev This emits when the approved address for an NFT is changed or
     ///  reaffirmed. The zero address indicates there is no approved address.
     ///  When a Transfer event emits, this also indicates that the approved
     ///  address for that NFT (if any) is reset to none.
-    event Approval(
-        address indexed _owner,
-        address indexed _approved,
-        uint256 indexed _tokenId
-    );
+    // event Approval(
+    //     address indexed _owner,
+    //     address indexed _approved,
+    //     uint256 indexed _tokenId
+    // );
 
     /// @dev This emits when an operator is enabled or disabled for an owner.
     ///  The operator can manage all NFTs of the owner.
-    event ApprovalForAll(
-        address indexed _owner,
-        address indexed _operator,
-        bool _approved
-    );
+    // event ApprovalForAll(
+    //     address indexed _owner,
+    //     address indexed _operator,
+    //     bool _approved
+    // );
 
     /// @notice Count all NFTs assigned to an owner
     /// @dev NFTs assigned to the zero address are considered invalid, and this
